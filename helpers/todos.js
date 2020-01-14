dfa32f6467addcc8aa0362dd37fcba417ca83bbd
ramon <ramondavi@msn.com>
2020-01-14 12:44:04 -0300

    todo list completed

--- helpers/todos.js
+++ helpers/todos.js
@@ -11,43 +11,43 @@ exports.getTodos = function(req, res){
 }
 
 exports.createTodo = function(req, res){
-    db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+  db.Todo.create(req.body)
+  .then(function(newTodo){
+      res.status(201).json(newTodo);
+  })
+  .catch(function(err){
+      res.send(err);
+  })
 }
 
 exports.getTodo = function(req, res){
-    db.Todo.findById(req.params.todoId)
-    .then(function(foundTodo){
-        res.json(foundTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+   db.Todo.findById(req.params.todoId)
+   .then(function(foundTodo){
+       res.json(foundTodo);
+   })
+   .catch(function(err){
+       res.send(err);
+   })
 }
 
-exports.updateTodo = function(req, res){
-    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-    .then(function(todo){
-        res.json(todo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+exports.updateTodo =  function(req, res){
+   db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+   .then(function(todo){
+       res.json(todo);
+   })
+   .catch(function(err){
+       res.send(err);
+   })
 }
 
 exports.deleteTodo = function(req, res){
-    db.Todo.remove({_id: req.params.todoId})
-    .then(function(){
-        res.json({message: 'We deleted it'});
-    })
-    .catch(function(err){
-        res.send(err);
-    })
+   db.Todo.remove({_id: req.params.todoId}) 
+   .then(function(){
+       res.json({message: 'We deleted it!'});
+   })
+   .catch(function(err){
+       res.send(err);
+   })
 }
 
 module.exports = exports;
--- routes/todos.js
+++ routes/todos.js
@@ -1,15 +1,16 @@
+  
 var express = require('express');
 var router = express.Router();
-var db = require('../models');
-var helpers = require('../helpers/todos');
+var db = require("../models");
+var helpers = require("../helpers/todos");
 
 router.route('/')
-.get(helpers.getTodos)
-.post(helpers.createTodo)
-
+ .get(helpers.getTodos)
+ .post(helpers.createTodo)
+ 
 router.route('/:todoId')
-.get(helpers.getTodo)
-.put(helpers.updateTodo)
-.delete(helpers.deleteTodo)
-
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo)
+  
 module.exports = router;
