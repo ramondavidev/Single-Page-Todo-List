c64e9849618e32cb33bbe1fa04662b787d1e644f
ramon <ramondavi@msn.com>
2020-01-13 21:45:22 -0300

    defining the routes

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000
 
+var todoRoutes = require('./routes/todos');
+
 app.get('/', (req, res) =>{
-    res.send("{data: 123141}");
+    res.send("HELLO FROM THE ROOT ROUTE");
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
     console.log("APP IS RUNNING ON PORT" + process.env.PORT);
