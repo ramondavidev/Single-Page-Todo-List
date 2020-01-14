2e05ebb434c437c045c314db256ba65920548382
ramon <ramondavi@msn.com>
2020-01-13 22:41:58 -0300

    CRUD todo

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express'),
 app = express(),
-port = process.env.PORT || 3000
+port = process.env.PORT || 3000,
+bodyParser = require('body-parser');
 
 var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) =>{
     res.send("HELLO FROM THE ROOT ROUTE");
